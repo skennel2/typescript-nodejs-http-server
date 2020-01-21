931e0826a997b3ebde818b24dc2f05203293b2c4
skennel2 <skennel2@gmail.com>
2020-01-21 09:52:24 +0900

    first commit

---
+++ dist/basicServer.js
@@ -0,0 +1,18 @@
+"use strict";
+var __importStar = (this && this.__importStar) || function (mod) {
+    if (mod && mod.__esModule) return mod;
+    var result = {};
+    if (mod != null) for (var k in mod) if (Object.hasOwnProperty.call(mod, k)) result[k] = mod[k];
+    result["default"] = mod;
+    return result;
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+var http = __importStar(require("http"));
+var server = http.createServer();
+server.on('request', function (request, response) {
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+    response.end('<h1>' + request.url + '</h1>');
+});
+server.listen(8888);
---
+++ src/basicServer.ts
@@ -0,0 +1,13 @@
+import * as http from 'http';
+
+let server = http.createServer();
+
+server.on('request', (request: http.IncomingMessage, response: http.ServerResponse) => {
+    response.writeHead(200, {
+        'Content-Type': 'text/html',
+    });
+
+    response.end('<h1>' + request.url + '</h1>');
+});
+
+server.listen(8888);
