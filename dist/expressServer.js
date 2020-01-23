4b55ab15f368ec65b52949bda9dd88ddae79eda9
skennel2 <skennel2@gmail.com>
2020-01-23 13:35:34 +0900

    express server test

--- dist/expressServer.js
+++ dist/expressServer.js
@@ -5,11 +5,21 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 var express_1 = __importDefault(require("express"));
 var app = express_1.default();
-app.use(function (request, response) {
-    response.writeHead(200, {
-        'Content-Type': 'text/html',
+app.use(function (request, response, next) {
+    console.log('accept request');
+    next();
+});
+app.get("/", function (request, response) {
+    response.end('<h1>default<h1>');
+});
+app.get("/hello", function (request, response) {
+    response.end('<h1>hello</h1>');
+});
+app.route('/api')
+    .get(function (request, response) {
+    response.json({
+        name: 'na yuns su'
     });
-    response.end('<h1>hello world</h1>');
 });
 app.listen(8888, function () {
     console.log('listening 8888 port...');
--- src/expressServer.ts
+++ src/expressServer.ts
@@ -1,16 +1,30 @@
 import express, { Response, Request } from 'express';
+import { NextFunction } from 'connect';
 
 var app = express();
+app.use((request: Request, response: Response, next: NextFunction) => {
+    console.log('accept request');
+    next();
+})
 
-app.use((request: Request, response: Response) => {
-    response.writeHead(200, {
-        'Content-Type': 'text/html',
-    });
-    
-    response.end('<h1>hello world</h1>');
+app.get("/", (request: Request, response: Response) => {
+    response.end('<h1>default<h1>');
 });
+
+app.get("/hello", (request: Request, response: Response) => {
+    response.end('<h1>hello</h1>');
+});
+
+//17 18 19 20
+
+app.route('/api')
+    .get((request: Request, response: Response) => {
+        response.json({
+            name: 'na yuns su'
+        });
+    })
+    
 
 app.listen(8888, () => {
     console.log('listening 8888 port...');
-})
-
+});
